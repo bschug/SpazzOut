759c6d01678cc2d16d0cf2eac4ed5385adb5eeed
Marc Zaku <mzaku@hotmail.de>
2015-01-24 19:13:51 +0100

    Adds ArmControls.

---
+++ Assets/Scripts/Input/ArmCtr.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmCtr : LimbCtr {
+
+	public override void UpdateAxes(Vector3 axes)
+	{
+		// no base call
+		upperLimb.AddRelativeTorque (new Vector3 (0, axes.x*sensitivity, 0));
+		lowerLimb.AddRelativeTorque (new Vector3 (0, axes.y*sensitivity, 0));
+	}
+}

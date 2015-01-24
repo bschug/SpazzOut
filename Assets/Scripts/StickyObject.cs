7deb8f065c3dee2de90953506a1f66bb733896c3
Benjamin Schug <benjamin.schug@gmail.com>
2015-01-24 19:58:54 +0100

    GravityButton now activates only when pushed by the player

--- Assets/Scripts/StickyObject.cs
+++ Assets/Scripts/StickyObject.cs
@@ -16,7 +16,7 @@ public class StickyObject : MonoBehaviour
 		if (joint != null) return;
 
 		foreach (var contact in collision.contacts) {
-			if (IsAttachedTo (player, contact.otherCollider.transform)) {
+			if (Utils.IsAttachedTo (player, contact.otherCollider.transform)) {
 				Debug.Log ("ATTACH JOINT");
 				joint = this.gameObject.AddComponent<FixedJoint> ();
 				joint.anchor = contact.point;
@@ -34,12 +34,5 @@ public class StickyObject : MonoBehaviour
 		joint.connectedBody = null;
 		Object.Destroy (joint);
 		joint = null;
-	}
-
-	bool IsAttachedTo (Transform parent, Transform obj)
-	{
-		if (obj.parent == null) return false;
-		if (obj.parent == parent) return true;
-		return IsAttachedTo (parent, obj.parent);
 	}
 }

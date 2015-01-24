92dae5d6c71cf3b8c7eea7f87028cc65588a83d9
Benjamin Schug <benjamin.schug@gmail.com>
2015-01-24 02:17:54 +0100

    moved scripts to subdirectory

---
+++ Assets/Scripts/CameraCtr.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraCtr : MonoBehaviour 
+{
+    public Transform Player;
+
+    public float Height = 2;
+    public int PositionDamping = 20;
+
+    List<Vector3> lastPlayerPositions = new List<Vector3> ();
+
+	void Start () {
+        for (int i = 0; i < PositionDamping; i++) {
+            lastPlayerPositions.Add (Player.position);
+        }
+
+		RingBuffer<int>.Test ();
+	}
+
+    void Update ()
+    {
+	    
+	}
+}

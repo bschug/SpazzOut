a29747b4940b1456bb02c38dc8b536c0c32c4b3a
moe-ped <m.berning@web.de>
2015-01-25 16:41:03 +0100

    Cat AI

---
+++ Assets/Scripts/CatAI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatAI : MonoBehaviour {
+
+	public float directionChangeCooldown;
+	float directionChangeTimer = 5;
+
+	Vector3 direction = Vector3.zero;
+
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		directionChangeTimer -= Time.deltaTime;
+		if (directionChangeTimer < 0)
+		{
+			changeDirection ();
+			directionChangeTimer = directionChangeCooldown;
+		}
+	}
+
+	void changeDirection ()
+	{
+		direction = new Vector3 (Random.value-0.5f, 0, Random.value-0.5f);
+		move ();
+	}
+
+	void move ()
+	{
+		rigidbody.velocity = direction.normalized * speed;
+		transform.forward = direction;
+	}
+}
